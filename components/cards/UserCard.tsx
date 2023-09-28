b598160b7c17129da2337f337dc17e02b9acfa8f
SnickerDev <64985447+sohan9819@users.noreply.github.com>
2023-09-28 12:32:34 +0530

    search page added

---
+++ components/cards/UserCard.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import { Button } from '../ui/button';
+import { useRouter } from 'next/navigation';
+
+type UserCardProps = {
+  key: string;
+  id: string;
+  name: string;
+  username: string;
+  imgUrl: string;
+  personType: string;
+};
+
+const UserCard = ({
+  key,
+  id,
+  name,
+  username,
+  imgUrl,
+  personType,
+}: UserCardProps) => {
+  const router = useRouter();
+
+  return (
+    <article className='user-card'>
+      <div className='user-card_avatar'>
+        <Image
+          src={imgUrl}
+          alt='avatar'
+          width={48}
+          height={48}
+          className='rounded-full object-cover aspect-square'
+        />
+
+        <div className='flex-1 text-ellipsis'>
+          <h4 className='text-base-semibold text-light-1'>{name}</h4>
+          <p className='text-small-medium text-gray-1'>@{username}</p>
+        </div>
+      </div>
+
+      <Button
+        className='user-card_btn'
+        onClick={() => router.push(`/profile/${id}`)}
+      >
+        View
+      </Button>
+    </article>
+  );
+};
+
+export default UserCard;
